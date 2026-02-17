0870fc4732803a926da79265073d5764545464ca
Garrett Lane <garrett.lane@outlook.de>
2026-02-17 14:59:32 +0100

    create index.html styles.css script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book(title, author) {
+  this.title = title;
+  this.author = author;
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
